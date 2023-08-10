ecba25d49600861b37e5398d273e7a60b1bdad1b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-08-10 17:40:00 -0300

    Requisito 04 concluido, testando rotas inexistentes

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Testando rota inexistente', async () => {
+  renderWithRouter(<App />, { route: '/xablau' });
+  const notFoundText = /page requested not found/i;
+  const imgAltText = /Clefairy pushing buttons randomly with text I have no idea what i'm doing/i;
+
+  const pageNotFound = await screen.findByRole('heading', { level: 2, name: notFoundText });
+
+  const imgNotFound = await screen.findByAltText(imgAltText);
+
+  expect(pageNotFound).toBeInTheDocument();
+  expect(imgNotFound).toBeInTheDocument();
+});
